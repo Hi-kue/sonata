bf2ada4eed6950f38435d87836757c7b539ddc41
Muhammad Bilal Khan <lofi.audit@gmail.com>
2025-06-18 01:53:42 -0400

    ref: footer w/version number

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://github.com/HAP-Archive/hap-archive-docs">Hap Archive v{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
